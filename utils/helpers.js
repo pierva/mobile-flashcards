019ba34affb0c702d49c298fb9bb60dcdf96640a
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-07-05 21:09:25 -0400

    feat: Add generateUUID helper method

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+/**
+ * @name generateUID
+ * This function will generate a UUID with the follow structure:
+ * 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+ */
+
+function generateUUID(){
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (dt + Math.random()*16)%16 | 0;
+      dt = Math.floor(dt/16);
+      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+}
