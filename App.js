8e6bd4be535124b389dcd63ace6f552d877fd2e9
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-07-16 21:43:06 -0400

    feat: Add Navigation

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
 import middleware from './middleware'
 import Constants from 'expo-constants'
-
-import { grey } from './utils/colors'
+import { Ionicons } from '@expo/vector-icons'
+import { grey, blue, white, lightBlue } from './utils/colors'
 
 import Home from './components/Home'
+import AddDeck from './components/AddDeck'
 
 const store = createStore(reducers, middleware)
 
@@ -21,11 +27,61 @@ function CustomStatusBar({ backgroundColor, ...props }) {
   )
 }
 
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
+
+const tabBarOptions = {
+  activeTintColor: Platform.OS === 'ios' ? blue : white,
+  inactiveTingColor: lightBlue,
+  style: {
+    height: 80,
+    backgroundColor: Platform.OS === 'ios' ? white : blue,
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1
+  }
+}
+
+const stackNavOptions = {
+  headerTintColor: white,
+  headerStyle: {
+    backgroundColor: blue,
+  }
+} 
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        itabBarIcon: ({ focused, color }) => {
+          if (route.name === 'Home') {
+            return <Ionicons name='ios-home-outline' size={30} color={color} />
+          } else if (route.name === 'Add Deck') {
+            return <Ionicons name='ios-add' size={30} color={color} />
+          }
+        }
+      })}
+      tabBarOptions={tabBarOptions}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false}} />
+      <Tab.Screen name="Add Deck" component={AddDeck} options={{ headerShown: false}} />
+    </Tab.Navigator>
+    
+  )
+}
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <CustomStatusBar backgroundColor={grey} barStyle='light-content' /> 
-      <Home />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <CustomStatusBar backgroundColor={grey} barStyle='light-content' /> 
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Tabs} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
   );
 }
---
+++ components/AddDeck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {View, Text } from 'react-native'
+
+
+export default class AddDeck extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the Add Deck component</Text>
+      </View>
+    )
+  }
+}
