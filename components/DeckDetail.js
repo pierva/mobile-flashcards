84877f9fa796d30e8093a6f86a055898447617cf
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-07-22 21:39:44 -0400

    feat: Style DeckDetail view

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,17 +1,73 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
+import { white, lightBlue, danger, lightGreen, blue } from '../utils/colors'
+
+import TextButton from './TextButton'
 
 class DeckDetail extends Component {
   render() {
     return (
-      <View>
-        <Text>This is the deck detail</Text>
-        <Text>{this.props.cards}</Text>
+      <View style={styles.container}>
+        <View style={styles.subContainer}>
+        <Text style={styles.header}>
+          {this.props.cards}
+          {this.props.cards === 1 ? ' Card' : ' Cards'}
+        </Text>
+        </View>
+        <View style={styles.subContainer}>
+          <TouchableOpacity 
+            style={[styles.button, {backgroundColor: lightGreen}]} 
+            onPress={console.log('pressed')}>
+            <Text style={styles.buttonText}>Add Card</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.button, {backgroundColor: blue}]} 
+            onPress={console.log('pressed')}>
+            <Text style={styles.buttonText}>Start Quiz</Text>
+          </TouchableOpacity>
+          <TextButton style={{color: danger, padding: 5}}>
+            Delete DeckDetail
+          </TextButton>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: "space-around"
+  },
+  header: {
+    fontSize: 24,
+    color: lightBlue,
+    marginTop: 30
+  },
+  subContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  button: {
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 5,
+    margin: 5,
+    width: 200
+  },
+
+  buttonText: {
+    width: '100%',
+    textAlign: "center",
+    color: white
+  }
+})
 
 function mapStateToProps(state, {route}) {
   const { deckId, deckName, cards } = route.params
